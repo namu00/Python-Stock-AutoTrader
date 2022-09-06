1f583fe6f591fd71ff287dd6d67250c3d1770ef1
skagns <skagns3571@naver.com>
2022-09-06 12:50:40 +0900

    Update Testfile

--- StockAuto.py
+++ StockAuto.py
@@ -5,7 +5,6 @@ import time
 import yaml
 
 def get_access_token():
-    
     headers = {"content-type":"application/json"}
     body = {"grant_type":"client_credentials",
     "appkey":APP_KEY, 
@@ -17,7 +16,6 @@ def get_access_token():
     return ACCESS_TOKEN
     
 def hashkey(datas):
-    
     PATH = "uapi/hashkey"
     URL = f"{URL_BASE}/{PATH}"
     headers = {
@@ -30,7 +28,6 @@ def hashkey(datas):
     return hashkey
 
 def get_current_price(code="005930"):
-    
     PATH = "uapi/domestic-stock/v1/quotations/inquire-price"
     URL = f"{URL_BASE}/{PATH}"
     headers = {"Content-Type":"application/json", 
@@ -67,7 +64,132 @@ def get_target_price(code="005930"):
     target_price = stck_oprc + (stck_hgpr - stck_lwpr) * 0.5
     return target_price
 
+def get_stock_balance():
+    PATH = "uapi/domestic-stock/v1/trading/inquire-balance"
+    URL = f"{URL_BASE}/{PATH}"
+    headers = {"Content-Type":"application/json", 
+        "authorization":f"Bearer {ACCESS_TOKEN}",
+        "appKey":APP_KEY,
+        "appSecret":APP_SECRET,
+        "tr_id":"TTTC8434R",
+        "custtype":"P",
+    }
+    params = {
+        "CANO": CANO,
+        "ACNT_PRDT_CD": ACNT_PRDT_CD,
+        "AFHR_FLPR_YN": "N",
+        "OFL_YN": "",
+        "INQR_DVSN": "02",
+        "UNPR_DVSN": "01",
+        "FUND_STTL_ICLD_YN": "N",
+        "FNCG_AMT_AUTO_RDPT_YN": "N",
+        "PRCS_DVSN": "01",
+        "CTX_AREA_FK100": "",
+        "CTX_AREA_NK100": ""
+    }
+    res = requests.get(URL, headers=headers, params=params)
+    stock_list = res.json()['output1']
+    evaluation = res.json()['output2']
+    stock_dict = {}
+    print("====주식 보유잔고====")
+    for stock in stock_list:
+        if int(stock['hldg_qty']) > 0:
+            stock_dict[stock['pdno']] = stock['hldg_qty']
+            print({stock['prdt_name']}, ({stock['pdno']}), ": ", {stock['hldg_qty']}"주")
+            time.sleep(0.1)
+    print("주식 평가 금액: ",{evaluation[0]['scts_evlu_amt']}, "원")
+    time.sleep(0.1)
+    print("평가 손익 합계: ", {evaluation[0]['evlu_pfls_smtl_amt']}, "원")
+    time.sleep(0.1)
+    print("총 평가 금액: ", {evaluation[0]['tot_evlu_amt']}, "원")
+    time.sleep(0.1)
+    print("=================")
+    return stock_dict
+
+def get_balance():
+    PATH = "uapi/domestic-stock/v1/trading/inquire-psbl-order"
+    URL = f"{URL_BASE}/{PATH}"
+    headers = {"Content-Type":"application/json", 
+        "authorization":f"Bearer {ACCESS_TOKEN}",
+        "appKey":APP_KEY,
+        "appSecret":APP_SECRET,
+        "tr_id":"TTTC8908R",
+        "custtype":"P",
+    }
+    params = {
+        "CANO": CANO,
+        "ACNT_PRDT_CD": ACNT_PRDT_CD,
+        "PDNO": "005930",
+        "ORD_UNPR": "65500",
+        "ORD_DVSN": "01",
+        "CMA_EVLU_AMT_ICLD_YN": "Y",
+        "OVRS_ICLD_YN": "Y"
+    }
+    res = requests.get(URL, headers=headers, params=params)
+    cash = res.json()['output']['ord_psbl_cash']
+    print("주문 가능 현금 잔고: ", {cash}, " 원")
+    return int(cash)
+
+def buy(code="005930", qty="1"):
+    """주식 시장가 매수"""  
+    PATH = "uapi/domestic-stock/v1/trading/order-cash"
+    URL = f"{URL_BASE}/{PATH}"
+    data = {
+        "CANO": CANO,
+        "ACNT_PRDT_CD": ACNT_PRDT_CD,
+        "PDNO": code,
+        "ORD_DVSN": "01",
+        "ORD_QTY": str(int(qty)),
+        "ORD_UNPR": "0",
+    }
+    headers = {"Content-Type":"application/json", 
+        "authorization":f"Bearer {ACCESS_TOKEN}",
+        "appKey":APP_KEY,
+        "appSecret":APP_SECRET,
+        "tr_id":"TTTC0802U",
+        "custtype":"P",
+        "hashkey" : hashkey(data)
+    }
+    res = requests.post(URL, headers=headers, data=json.dumps(data))
+    if res.json()['rt_cd'] == '0':
+        print("[매수 성공] ", {str(res.json())})
+        return True
+    else:
+        print("[매수 실패] ", {str(res.json())})
+        return False
+
+def sell(code="005930", qty="1"):
+    """주식 시장가 매도"""
+    PATH = "uapi/domestic-stock/v1/trading/order-cash"
+    URL = f"{URL_BASE}/{PATH}"
+    data = {
+        "CANO": CANO,
+        "ACNT_PRDT_CD": ACNT_PRDT_CD,
+        "PDNO": code,
+        "ORD_DVSN": "01",
+        "ORD_QTY": qty,
+        "ORD_UNPR": "0",
+    }
+    headers = {"Content-Type":"application/json", 
+        "authorization":f"Bearer {ACCESS_TOKEN}",
+        "appKey":APP_KEY,
+        "appSecret":APP_SECRET,
+        "tr_id":"TTTC0801U",
+        "custtype":"P",
+        "hashkey" : hashkey(data)
+    }
+    res = requests.post(URL, headers=headers, data=json.dumps(data))
+    if res.json()['rt_cd'] == '0':
+        print("[매도 성공] ",{str(res.json())})
+        return True
+    else:
+        print("[매도 실패] ",{str(res.json())})
+        return False
+
 if __name__ == " __main__":
+    now = datetime.datetime.now()
+    
+
     with open('config.yaml',encoding = 'utf-8') as f:
         config = yaml.load(f,Loader = yaml.FullLoader)
     APP_KEY = config['APP_KEY']
